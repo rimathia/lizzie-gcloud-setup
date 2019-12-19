3b9bf88b6ee38b7684bc05c35664aaaa4a24a6c0
rimathia <rimathia@users.noreply.github.com>
2019-12-19 22:59:18 +0100

    add forgotten -y

--- remote_katago/download-katago-network.sh
+++ remote_katago/download-katago-network.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://github.com/lightvector/KataGo/releases/download/v1.1/g104-b20c256-s447913472-d241840887.zip
-sudo apt install unzip
+sudo apt install -y unzip
 unzip g104-b20c256-s447913472-d241840887.zip 
 rm g104-b20c256-s447913472-d241840887.zip 
