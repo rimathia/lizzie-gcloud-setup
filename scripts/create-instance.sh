5030b6fd663826604520ea374c9cb3d93cc58703
rimathia <rimathia@users.noreply.github.com>
2019-12-19 18:59:40 +0100

    add preemptible option to config.sh

--- scripts/create-instance.sh
+++ scripts/create-instance.sh
@@ -14,7 +14,7 @@ echo "       --accelerator type=$GPU_TYPE,count=$GPU_COUNT"
 echo "       --boot-disk-size $DISK_SIZE"
 echo "       --image-family=$IMAGE_FAMILY"
 echo "       --image-project=$IMAGE_PROJECT"
-echo "       --preemptible"
+echo "       $PREEMPTIBLE"
 
 # Usually no need to touch this
 gcloud compute instances create "$INSTANCE_NAME" \
@@ -25,4 +25,4 @@ gcloud compute instances create "$INSTANCE_NAME" \
        --boot-disk-size "$DISK_SIZE" \
        --image-family="$IMAGE_FAMILY" \
        --image-project="$IMAGE_PROJECT" \
-       --preemptible
+       $PREEMPTIBLE
